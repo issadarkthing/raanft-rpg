ee017ba11f3a8883fc451dd372ad1acf4dd10ab5
raziman <raziman.terra@gmail.com>
2022-02-16 21:32:21 +0800

    update monster names

--- src/structure/Monster.ts
+++ src/structure/Monster.ts
@@ -14,11 +14,20 @@ export class Monster extends Fighter {
   private enableImage = false;
   private random: Random;
   
-  constructor(name: string, imageUrl: string, index: number) {
-    super(name);
-    const SEED = index;
-    this.random = new Random(MersenneTwister19937.seed(SEED));
-    this.difficulty = index + 1;
+  constructor(player: Player) {
+    super("");
+    const nameIndex = random.integer(0, names.length - 1);
+    const levelIndex = random.integer(0, levels.length - 1);
+    const name = names[nameIndex];
+    const level = levels[levelIndex];
+
+    this.name = `${level} ${name}`;
+    this.difficulty = player.level;
+    this.attack = player.attack + this.randomAttrib();
+    this.hp = player.hp + this.randomAttrib();
+    this.armor = player.armor + (this.randomAttrib() / 100);
+    this.critChance = player.critChance + (this.randomAttrib() / 100);
+    this.critDamage = player.critDamage + random.integer(0.01, 0.5);
 
     if (this.enableImage) {
       this.imageUrl = imageUrl;
@@ -61,3 +70,108 @@ export class Monster extends Fighter {
     return profile;
   }
 }
+
+const levels = [
+  "Weak",
+  "Strong",
+  "Armoured",
+];
+
+const names = [
+  "Fish", 
+  "Crab", 
+  "Scorpion", 
+  "Bird", 
+  "Boar", 
+  "Wolves", 
+  "Bear", 
+  "Orc", 
+  "Soldier", 
+  "Henchmen", 
+  "Monster", 
+  "Zombie", 
+  "Goblins", 
+  "Mercenary", 
+  "Undead", 
+  "Orcs", 
+  "Creeper", 
+  "Reaper",
+  "Mole",
+  "Crab",
+  "Chicken",
+  "Cockroach",
+  "Duck",
+  "Bee Swarm",
+  "Angry Sheep",
+  "Cow",
+  "Cobra Queen",
+  "Zombie Cow",
+  "Rat",
+  "Seagull",
+  "Rabbit",
+  "Evil Peasant",
+  "Baby Orc",
+  "Giant Spider",
+  "Hyena",
+  "Giant Rat",
+  "Evil Flight Attendant",
+  "Dwarf",
+  "Goblin",
+  "Giant Chicken",
+  "Undead cow",
+  "Skeleton",
+  "Snake Nest",
+  "Soldier of the West",
+  "Undead Goblin",
+  "Warlock",
+  "Pack of Hyenas",
+  "Giant Skeleton",
+  "Evil Skeleton",
+  "Grave Scorpion",
+  "Jungle Snake",
+  "Summoner",
+  "Thug",
+  "Troll Runt",
+  "Priest",
+  "Summoned Zombie",
+  "Mega Thug",
+  "Tree Spirit",
+  "Minotaur",
+  "Imp Champion",
+  "Dark Warrior",
+  "Wizard",
+  "Thug Crew",
+  "Rogue Spirit",
+  "Band of Skeletons",
+  "Hound",
+  "Dark Wizard",
+  "Hand of Darkness",
+  "Water Wizard",
+  "Leprechaun Warrior",
+  "Blessed Giant Chicken",
+  "Fire Hound",
+  "Slime",
+  "Possessed Snakes",
+  "Cave Barbarian",
+  "Angry Barbarian",
+  "Chaotic Wizard",
+  "Titan",
+  "Evil Mage",
+  "Undead Hero",
+  "Titan",
+  "Swarm of Zombies",
+  "Dark Titan",
+  "High Priest",
+  "Baby Dragon",
+  "Troll General",
+  "Mountain Troll",
+  "Blessed Hero",
+  "Titanic Monster",
+  "Paladin",
+  "Death Unicorn",
+  "Possessed Dragon",
+  "Dragon Queen",
+  "Undead Paladin",
+  "Gargoyle Prince",
+  "Evil Slime" ,
+];
