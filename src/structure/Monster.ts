97704b2edb05dff5911f99632e14815111a63650
raziman <raziman.terra@gmail.com>
2022-02-17 15:04:08 +0800

    fix coin and xp drop to high

--- src/structure/Monster.ts
+++ src/structure/Monster.ts
@@ -23,8 +23,8 @@ export class Monster extends Fighter {
 
     this.name = `${level} ${name}`;
     this.difficulty = player.level;
-    this.drop = random.integer(150, 500) * this.difficulty;
-    this.xpDrop = random.integer(10, 35) * this.difficulty;
+    this.drop = random.integer(150, 500) * Math.round(1 + (this.difficulty / 10));
+    this.xpDrop = random.integer(10, 35) * Math.round(1 + (this.difficulty / 10))
     this.attack = player.attack + this.randomAttrib();
     this.hp = player.hp + this.randomAttrib();
     this.armor = player.armor + (this.randomAttrib() / 100);
