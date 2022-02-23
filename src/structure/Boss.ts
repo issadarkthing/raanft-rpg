d7e618701cba67f8a7b549edaf01d3130ec7f0df
raziman <raziman.terra@gmail.com>
2022-02-23 08:57:09 +0800

    change boss names

--- src/structure/Boss.ts
+++ src/structure/Boss.ts
@@ -1,90 +1,23 @@
-import { Fighter } from "@jiman24/discordjs-rpg";
-import { currency } from "../utils";
-import { Defense, Heal, FlameEyes } from "../structure/Skill";
-import { Phoenix, Slime, Titan } from "./Pet";
+import { Monster } from "./Monster";
 
-export abstract class Boss extends Fighter {
-  abstract drop: number;
-  abstract xpDrop: number;
-
+export class Boss extends Monster {
   static get all(): Boss[] {
-    return [
-      new Cavernmonster("Manzana Banana"),
-      new Vortexscreamer("Burro Banana"),
-      new Rottingseeker("Barangan Banana"),
-    ];
-  }
-
-  show() {
-    const embed = super.show();
-
-    embed.addField(`${currency} Drop`, `${this.drop}`, true);
-    embed.addField(`XP Drop`, `${this.xpDrop}`, true);
-
-    return embed;
+    return names.map((x, i) => new Monster(x, "", i * 20));
   }
 }
 
-export class Cavernmonster extends Boss {
-  drop = 12000;
-  xpDrop = 500;
-  attack = 500;
-  hp = 550;
-  armor = 0.3;
-  critChance = 0.1;
-  critDamage = 3;
-  imageUrl = "https://www.runehq.com/image/monsterdb/v/vanstromklause.png";
-  
-  constructor(name: string) {
-    super(name);
-
-    const skill = new Heal(); 
-    skill.setOwner(this);
-
-    const pet = new Slime()
-    pet.setOwner(this);
-  }
-}
-
-export class Vortexscreamer extends Boss {
-  drop = 24000;
-  xpDrop = 800;
-  attack = 1200;
-  hp = 1420;
-  armor = 0.35;
-  critChance = 0.2;
-  critDamage = 3.4;
-  imageUrl = "https://www.runehq.com/image/monsterdb/k/kriltsutsaroth.png"
-
-  constructor(name: string) {
-    super(name);
-
-    const skill = new FlameEyes(); 
-    skill.setOwner(this);
-
-    const pet = new Phoenix()
-    pet.setOwner(this);
-  }
-}
-
-export class Rottingseeker extends Boss {
-  drop = 35000;
-  xpDrop = 1000;
-  attack = 2350;
-  hp = 2570;
-  armor = 0.39;
-  critChance = 0.2;
-  critDamage = 3.8;
-  imageUrl = "https://www.runehq.com/image/monsterdb/k/kreearra.png";
-
-  constructor(name: string) {
-    super(name);
-
-    const skill = new Defense(); 
-    skill.setOwner(this);
-
-    const pet = new Titan()
-    pet.setOwner(this);
-  }
-}
-
+const names = [
+  "Grinch",
+  "Kraken",
+  "Basilisk",
+  "King Kong",
+  "Mike Wazowski",
+  "Loch Ness Monster",
+  "Alien Steve",
+  "Terminator Robot",
+  "Centaur Legion",
+  "Phoenix Emperor",
+  "Nazgul",
+  "Angel Queen",
+  "Demon King",
+];
